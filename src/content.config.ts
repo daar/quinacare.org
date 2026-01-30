952fe76c285992972b69269ca5003d2b87f0b6fd
Darius Blaszyk <dhkblaszyk@gmail.com>
2026-01-30 21:22:27 +0100

    migrate blog-roll, rename blog to actueel

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,19 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-const blog = defineCollection({
-  loader: glob({ pattern: '**/*.mdoc', base: './src/content/blog' }),
+const actueel = defineCollection({
+  loader: glob({ pattern: '**/*.mdoc', base: './src/content/actueel' }),
   schema: z.object({
     title: z.string(),
     date: z.date(),
+    status: z.string().optional(),
+    slug: z.string().optional(),
+    author: z.string().optional(),
+    excerpt: z.string().optional(),
+    categories: z.array(z.string()).optional(),
+    language: z.string().optional(),
+    featured_image: z.string().optional(),
   }),
 });
 
-export const collections = { blog };
+export const collections = { actueel };
